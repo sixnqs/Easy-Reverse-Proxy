042a831a125fc2ac75d666a86f2eca2422fff6ab
sixnqs <115446832+sixnqs@users.noreply.github.com>
2024-09-23 09:41:44 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,4 +11,8 @@ export interface Proxy {
 }
 
 // 代理
-export let PROXYS = [];
+export let PROXYS = [    {
+        domain: "www.example1.com",
+        url: "https://www.microsoft.com/",
+        enable: true
+    }];
